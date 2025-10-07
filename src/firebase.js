86fe065eac32752d902e1ec4492fee7548b1e8c5
sihht123 <sih.ht123@gmail.com>
2025-10-07 13:37:30 +0100

    Initial commit - Roadmap app

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import Firebase
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// Your Firebase configuration
+// REPLACE THIS with your config from Step 14!
+const firebaseConfig = {
+   apiKey: "AIzaSyBYL1uWE0BTZLtTn9ZXMih6qmfkbdYWJDA",
+  authDomain: "ecommerce-roadmap.firebaseapp.com",
+  projectId: "ecommerce-roadmap",
+  storageBucket: "ecommerce-roadmap.firebasestorage.app",
+  messagingSenderId: "994662930339",
+  appId: "1:994662930339:web:b2779c6c06729883b66c16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Export for use in other files
+export { db, auth };
