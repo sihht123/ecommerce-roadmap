fd6312344a8ff76f95118745593e97eb1b252bfa
sihht123 <sih.ht123@gmail.com>
2025-10-09 14:06:16 +0100

    Add minimal App component with default export

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { auth, db } from './firebase';
+//import React, { useState, useEffect } from 'react';
+//import { auth, db } from './firebase';
 /*import { 
   signInWithEmailAndPassword, 
   signOut, 
